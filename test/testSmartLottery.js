1d470d9abf62d3b6129b8dc92b9f4e304acf5230
pklinger <philipp@code.business>
2019-06-04 17:40:04 +0200

    adapted tests for web3 v1.0

--- test/testSmartLottery.js
+++ test/testSmartLottery.js
@@ -2,9 +2,9 @@ var SmartLottery = artifacts.require("./SmartLottery.sol");
 
 var log = console.log;
 
-contract('Smart Lottery', function(accounts) {
+contract('Smart Lottery', function (accounts) {
 
-    describe('Smart Lottery walkthrough', function() {
+    describe('Smart Lottery walkthrough', async () => {
 
         var lotteryContract = null;
         var participants = [
@@ -16,12 +16,12 @@ contract('Smart Lottery', function(accounts) {
             '0x0000000000000000000000000000000000000000'
         ];
 
-        before(function(done) {
+        before(function (done) {
             SmartLottery.new({
                 from: accounts[0]
             })
                 .catch(log)
-                .then(function(contract) {
+                .then(function (contract) {
                     lotteryContract = contract;
                 })
                 .then(done);
@@ -32,14 +32,14 @@ contract('Smart Lottery', function(accounts) {
          */
         function testSmartLotteryInitialization() {
 
-            it("should have initialized SmartLottery", function() {
+            it("should have initialized SmartLottery", function () {
                 assert.notEqual(lotteryContract, null);
             });
 
 
-            it("should be empty in the beginning", function() {
+            it("should be empty in the beginning", function () {
                 return lotteryContract.getParticipants.call()
-                    .then(function(result) {
+                    .then(function (result) {
                         assert.deepEqual(result, participants, "there are already participants in the lottery.");
                     });
             });
@@ -56,90 +56,85 @@ contract('Smart Lottery', function(accounts) {
          * Smart Contract Functionality Tests
          */
 
-        describe('Smart Contract functionality', function() {
+        describe('Smart Contract functionality', async () => {
 
-            it("should be able to set a bet", function() {
-                return lotteryContract.bet(0, {
+            it("should be able to set a bet", async () => {
+
+                var tx = await lotteryContract.bet(0, {
                     from: accounts[0],
-                    value: web3.toWei(1, "ether")
-                })
-                    .then(function(tx) {
-                        assert.isOk(tx.receipt);
-                    });
-            });
-
-            it("should have a participant", function() {
-                return lotteryContract.getParticipants.call()
-                    .then(function(result) {
-                        var tempParticipants = participants.slice();
-                        tempParticipants[0] = accounts[0];
-                        assert.deepEqual(result, tempParticipants, "participants not as expected");
-                    });
-            });
-
-            it("should pay for slot", function() {
-
-                var beforeTest = web3.fromWei(web3.eth.getBalance(accounts[0])).toString(10);
-                var afterTest = null;
-
-                lotteryContract.bet(1, {
-                    from: accounts[0],
-                    value: web3.toWei(1, "ether")
-                })
-                .then(function(tx) {
-                    afterTest = web3.fromWei(web3.eth.getBalance(accounts[0])).toString(10);
-                    console.log("After: " + afterTest);
-                    assert.isOk(tx.receipt);
+                    value: web3.utils.toWei("1", "ether")
                 });
+
+                return assert.isOk(tx.receipt);
+
+            });
+
+            it("should have a participant", async () => {
+                var participants = await lotteryContract.getParticipants.call();
+
+                var tempParticipants = participants.slice();
+                tempParticipants[0] = accounts[0];
+                return assert.deepEqual(participants, tempParticipants, "participants not as expected");
+
+            });
+
+            it("should pay for slot", async () => {
+
+                var beforeBalance = await web3.eth.getBalance(accounts[0]);
+
+                var beforeTest = await web3.utils.fromWei(beforeBalance.toString());
+
+                var tx = await lotteryContract.bet(1, {
+                    from: accounts[0],
+                    value: web3.utils.toWei("1", "ether")
+                });
+                assert.isOk(tx.receipt);
+
+                var afterBalance = await web3.eth.getBalance(accounts[0]);
+                var afterTest = await web3.utils.fromWei(afterBalance.toString());
 
                 return assert(afterTest < beforeTest, "did not pay for slot");
             });
 
-            it("should distribute pot after winning", function() {
+            it("should distribute pot after winning", async () => {
 
                 // fill further slots
-                return lotteryContract.bet(2, {
+                await lotteryContract.bet(2, {
                     from: accounts[0],
-                    value: web3.toWei(1, "ether")
-                })
-                    .then(function() {
-                        return lotteryContract.bet(3, {
-                            from: accounts[0],
-                            value: web3.toWei(1, "ether")
-                        })
-                    }).then(function() {
-                        return lotteryContract.bet(4, {
-                            from: accounts[0],
-                            value: web3.toWei(1, "ether")
-                        })
-                    }).then(function() {
-                        return lotteryContract.getPotValue();
-                    }).then(function(result) {
-                        // there should be ether in the contract
-                        assert.equal(result.toNumber(), web3.toWei(5, "ether"), "Pot is not empty.");
-                    }).then(function() {
-                        // the last slot will get all ether in the contract
-                        return lotteryContract.bet(5, {
-                            from: accounts[0],
-                            value: web3.toWei(1, "ether")
-                        })
-                    }).then(function() {
-                        return lotteryContract.getPotValue();
-                    }).then(function(result) {
-                        // and the contract is empty again
-                        assert.equal(result, 0, "Pot is empty.");
-                    });
+                    value: web3.utils.toWei("1", "ether")
+                });
+
+                await lotteryContract.bet(3, {
+                    from: accounts[0],
+                    value: web3.utils.toWei("1", "ether")
+                });
+
+                await lotteryContract.bet(4, {
+                    from: accounts[0],
+                    value: web3.utils.toWei("1", "ether")
+                });
+
+
+                var potValue = await lotteryContract.getPotValue();
+                assert.equal(potValue, web3.utils.toWei("5", "ether"), "Pot is not empty.");
+
+                await lotteryContract.bet(5, {
+                    from: accounts[0],
+                    value: web3.utils.toWei("1", "ether")
+                });
+
+
+                var newPotValue = await lotteryContract.getPotValue();
+                assert.equal(newPotValue, 0, "Pot is empty.");
 
             });
 
-            it("should reset the lottery after winner was chosen", function() {
-                return lotteryContract.getParticipants.call()
-                    .then(function(result) {
-                        assert.deepEqual(result, participants, "there should not be any participants");
-                        return lotteryContract.soldSlots.call();
-                    }).then(function(result) {
-                        assert.equal(result.toNumber(), 0, "there should not be any participants");
-                    });
+            it("should reset the lottery after winner was chosen", async () => {
+                var currentParticipants = await lotteryContract.getParticipants.call();
+                assert.deepEqual(currentParticipants, participants, "there should not be any participants");
+
+                var soldSlots = await lotteryContract.soldSlots.call();
+                assert.equal(soldSlots, 0, "there should not be any participants");
             });
 
         });
